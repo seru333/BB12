993edbe4f36123a3525780029bfab878281c56f4
seru333 <seru333@gmail.com>
2016-10-11 18:56:17 -0600

    ir and docking implemented

--- dual_Front_Test/dual_Front_Test.ino
+++ dual_Front_Test/dual_Front_Test.ino
@@ -9,15 +9,16 @@
 #define flse 4   //front left sensor echo
 #define fmst 8   //front mid sensor trig
 #define fmse 9   //front mid sensor echo
-#define irfs 23  //front IR reciever
+#define irfs A1  //front IR reciever
 
 //Proximity flags for ultrasonic sensors
 boolean lBlock = false;
 boolean fBlock = false;
+int IRread = 0;
 
 //IR pin assignment and variables
-IRrecv irrecv(irfs); //create an IRrecv object
-decode_results decodedSignal; //stores results from IR detector
+//IRrecv irrecv(irfs); //create an IRrecv object
+//decode_results decodedSignal; //stores results from IR detector
 
 void setup() {
   pinMode(lst, OUTPUT);
@@ -29,11 +30,12 @@ void setup() {
   pinMode(fmst, OUTPUT);
   pinMode(fmse, INPUT);
   pinMode(irfs, INPUT);
-  irrecv.enableIRIn();
+  //irrecv.enableIRIn();
+  set_fs();
 }
 
 void loop() {
-  do{
+  while (IRread > 100){
     set_fs();
     if (fBlock == false){
       motor_straight();
@@ -51,8 +53,8 @@ void loop() {
         } while (fBlock == true);
       }
     }
-  }while (irrecv.decode(&decodedSignal) == false);
-    docking();
+  }
+  docking();
 }
 
 void motor_straight() {
@@ -64,9 +66,9 @@ void motor_straight() {
 
 void motor_straight_slow() {
   digitalWrite(12,HIGH); 
-  analogWrite(3,150);
+  analogWrite(3,50);
   digitalWrite(13,LOW);
-  analogWrite(11,150);
+  analogWrite(11,50);
 }
 
 void motor_left() {
@@ -76,11 +78,24 @@ void motor_left() {
   analogWrite(11,200);
 }
 
+void motor_left_slow() {
+  digitalWrite(12,LOW); 
+  analogWrite(3,50);
+  digitalWrite(13,LOW);
+  analogWrite(11,50);
+}
 void motor_right() {
   digitalWrite(12,HIGH); 
   analogWrite(3,200);
   digitalWrite(13,HIGH);
   analogWrite(11,200);
+}
+
+void motor_right_slow() {
+  digitalWrite(12,HIGH); 
+  analogWrite(3,50);
+  digitalWrite(13,HIGH);
+  analogWrite(11,50);
 }
 
 void motor_stop() { 
@@ -106,7 +121,7 @@ void set_ls() {
 }
 
 void set_fs() {
-  irrecv.resume();
+  IRread = analogRead(irfs);
   long duration0, distance0;
   long duration1, distance1;
   long duration2, distance2;
@@ -151,7 +166,7 @@ void docking() {
   digitalWrite(frst, LOW);
   duration0 = pulseIn(frse, HIGH);
   distance0 = (duration0/2) / 29.1;
-  delay(10);
+  delay(5);
   digitalWrite(flst, LOW);
   delayMicroseconds(2);
   digitalWrite(flst, HIGH);
@@ -159,18 +174,23 @@ void docking() {
   digitalWrite(flst, LOW);
   duration2 = pulseIn(flse, HIGH);
   distance2 = (duration2/2) / 29.1;
-  if (distance0 > distance2){
-      motor_left();
-    }
-  else if (distance0 < distance2){
-    motor_right();
+  if (distance0 <= 10 && distance2 <= 10){
+    motor_stop();
+  }
+  else if (distance0 >= 50 || distance2 >= 50){
+    motor_straight(); 
   }
   else{
-    if (distance0 <= 10){
-      motor_stop();
+    if (round(distance0) > round(distance2) + 1){
+      motor_left_slow();
+      delay(100);
+    }
+    else if (round(distance0) + 1 < round(distance2)){
+      motor_right_slow();
+      delay(200);
     }
     else{
-        motor_straight_slow();  
+      motor_straight_slow();  
     }
   }
 }
